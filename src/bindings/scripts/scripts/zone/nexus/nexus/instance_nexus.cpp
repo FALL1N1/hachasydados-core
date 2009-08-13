cbd5f174a0846dee3834d3d3b0a87fc959d70b8b
megamage <megamage@localhost>
2009-08-13 16:15:22 -0500

    *Fix a typo that broken quest death's challenge

---
+++ src/bindings/scripts/scripts/zone/nexus/nexus/instance_nexus.cpp
@@ -0,0 +1,258 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: Instance_Nexus
+SD%Complete:
+SDComment:
+SDCategory: The Nexus, The Nexus
+EndScriptData */
+
+#include "precompiled.h"
+#include "def_nexus.h"
+
+#define NUMBER_OF_ENCOUNTERS      4
+
+struct TRINITY_DLL_DECL instance_nexus : public ScriptedInstance
+{
+    instance_nexus(Map *Map) : ScriptedInstance(Map) { Initialize(); }
+
+    uint32 Encounters[NUMBER_OF_ENCOUNTERS];
+
+    uint64 Anomalus;
+    uint64 Keristrasza;
+
+    uint64 AnomalusContainmentSphere;
+    uint64 OrmoroksContainmentSphere;
+    uint64 TelestrasContainmentSphere;
+
+    std::string strInstData;
+
+    void Initialize()
+    {
+        Anomalus = 0;
+        for(uint8 i = 0; i < NUMBER_OF_ENCOUNTERS; ++i)
+            Encounters[i] = NOT_STARTED;
+    }
+
+    void OnCreatureCreate(Creature* pCreature, bool add)
+    {
+        Map::PlayerList const& players = instance->GetPlayers();
+        uint32 TeamInInstance = 0;
+
+        if (!players.isEmpty())
+        {
+            if (Player* pPlayer = players.begin()->getSource())
+            {
+                TeamInInstance = pPlayer->GetTeam();
+            }
+        }
+        switch(pCreature->GetEntry())
+        {
+            case 26763: Anomalus = pCreature->GetGUID(); break;
+            case 26723: Keristrasza = pCreature->GetGUID(); break;
+            case 26800:
+                {
+                    //26799
+                    pCreature->setFaction(16);
+                    if (TeamInInstance == ALLIANCE)
+                        pCreature->SetDisplayId(24358);
+                    break;
+                }
+            case 26802:
+                {
+                    //26801
+                    pCreature->setFaction(16);
+                    if (TeamInInstance == ALLIANCE)
+                        pCreature->SetDisplayId(24354);
+                    break;
+                }
+            case 26805:
+                {
+                    //26803
+                    pCreature->setFaction(16);
+                    if (TeamInInstance == ALLIANCE)
+                        pCreature->SetDisplayId(24357);
+                    break;
+                }
+            case 27949:
+                {
+                    //27947
+                    pCreature->setFaction(16);
+                    if (TeamInInstance == ALLIANCE)
+                        pCreature->SetDisplayId(24352);
+                    break;
+                }
+            case 26796: 
+                {
+                    //26798
+                    pCreature->setFaction(16);
+                    if (TeamInInstance == ALLIANCE)
+                        pCreature->SetDisplayId(24352);
+                    break;
+                }
+        }
+    }
+
+    void OnGameObjectCreate(GameObject *go, bool add)
+    {
+        switch(go->GetEntry())
+        {
+            case 188527:
+            {
+                AnomalusContainmentSphere = go->GetGUID();
+                if(Encounters[1] == DONE)
+                    go->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
+                break;
+            }
+            case 188528:
+            {
+                OrmoroksContainmentSphere = go->GetGUID();
+                if(Encounters[2] == DONE)
+                    go->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
+                break;
+            }
+            case 188526:
+            {
+                TelestrasContainmentSphere = go->GetGUID();
+                if(Encounters[0] == DONE)
+                    go->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
+                break;
+            }
+        }
+    }
+
+    uint32 GetData(uint32 identifier)
+    {
+        switch(identifier)
+        {
+            case DATA_MAGUS_TELESTRA_EVENT: return Encounters[0];
+            case DATA_ANOMALUS_EVENT:       return Encounters[1];
+            case DATA_ORMOROK_EVENT:        return Encounters[2];
+            case DATA_KERISTRASZA_EVENT:    return Encounters[3];
+        }
+        return 0;
+    }
+
+    void SetData(uint32 identifier, uint32 data)
+    {
+        switch(identifier)
+        {
+            case DATA_MAGUS_TELESTRA_EVENT:
+            {
+                if(data == DONE)
+                {
+                    GameObject *Sphere = instance->GetGameObject(TelestrasContainmentSphere);
+                    if(Sphere)
+                        Sphere->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
+                }
+                Encounters[0] = data;
+                break;
+            }
+            case DATA_ANOMALUS_EVENT:
+            {
+                if(data == DONE)
+                {
+                    GameObject *Sphere = instance->GetGameObject(AnomalusContainmentSphere);
+                    if(Sphere)
+                        Sphere->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
+                }
+                Encounters[1] = data;
+                break;
+            }
+            case DATA_ORMOROK_EVENT:
+            {
+                if(data == DONE)
+                {
+                    GameObject *Sphere = instance->GetGameObject(OrmoroksContainmentSphere);
+                    if(Sphere)
+                        Sphere->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
+                }
+                Encounters[2] = data;
+                break;
+            }
+            case DATA_KERISTRASZA_EVENT:    Encounters[3] = data; break;
+        }
+
+        if (data == DONE)
+        {
+            OUT_SAVE_INST_DATA;
+
+            std::ostringstream saveStream;
+            saveStream << Encounters[0] << " " << Encounters[1] << " " << Encounters[2] << " "
+                << Encounters[3];
+
+            strInstData = saveStream.str();
+
+            SaveToDB();
+            OUT_SAVE_INST_DATA_COMPLETE;
+        }
+    }
+
+    uint64 GetData64(uint32 uiIdentifier)
+    {
+        switch(uiIdentifier)
+        {
+            case DATA_ANOMALUS:                 return Anomalus;
+            case DATA_KERISTRASZA:              return Keristrasza;
+            case ANOMALUS_CONTAINMET_SPHERE:    return AnomalusContainmentSphere;
+            case ORMOROKS_CONTAINMET_SPHERE:    return OrmoroksContainmentSphere;
+            case TELESTRAS_CONTAINMET_SPHERE:   return TelestrasContainmentSphere;
+        }
+        return 0;
+    }
+
+    std::string GetSaveData()
+    {
+        return strInstData;
+    }
+
+    void Load(const char* chrIn)
+    {
+        if (!chrIn)
+        {
+            OUT_LOAD_INST_DATA_FAIL;
+            return;
+        }
+
+        OUT_LOAD_INST_DATA(chrIn);
+
+        std::istringstream loadStream(chrIn);
+        loadStream >> Encounters[0] >> Encounters[1] >> Encounters[2] >> Encounters[3];
+
+        for(uint8 i = 1; i < NUMBER_OF_ENCOUNTERS; ++i)
+        {
+            if (Encounters[i] == IN_PROGRESS)
+                Encounters[i] = NOT_STARTED;
+        }
+
+        OUT_LOAD_INST_DATA_COMPLETE;
+    }
+};
+
+InstanceData* GetInstanceData_instance_nexus(Map* map)
+{
+    return new instance_nexus(map);
+}
+
+void AddSC_instance_nexus()
+{
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name = "instance_nexus";
+    newscript->GetInstanceData = &GetInstanceData_instance_nexus;
+    newscript->RegisterSelf();
+}
---
+++ src/bindings/scripts/scripts/zone/ulduar/ulduar/boss_flame_leviathan.cpp
@@ -0,0 +1,306 @@
+/*
+ * Copyright (C) 2008 - 2009 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "precompiled.h"
+#include "def_ulduar.h"
+#include "Vehicle.h"
+
+#define SPELL_PURSUED           62374
+#define SPELL_GATHERING_SPEED   62375
+#define SPELL_BATTERING_RAM     62376
+#define SPELL_FLAME_VENTS       62396
+#define SPELL_MISSILE_BARRAGE   62400
+#define SPELL_SYSTEMS_SHUTDOWN  62475
+
+#define SPELL_CANNON            62397
+
+#define SPELL_OVERLOAD_CIRCUIT  62399
+
+#define SPELL_SEARING_FLAME     62402
+
+enum Events
+{
+    EVENT_PURSUE = 1,
+    EVENT_MISSILE,
+    EVENT_VENT,
+};
+
+struct TRINITY_DLL_DECL boss_flame_leviathanAI : public BossAI
+{
+    boss_flame_leviathanAI(Creature *c) : BossAI(c, BOSS_LEVIATHAN)
+    {
+        assert(c->isVehicle());
+    }
+
+    void MoveInLineOfSight(Unit* who) {}
+
+    void EnterCombat(Unit *who)
+    {
+        _EnterCombat();
+        events.ScheduleEvent(EVENT_PURSUE, 0);
+        events.ScheduleEvent(EVENT_MISSILE, 1500);
+        events.ScheduleEvent(EVENT_VENT, 20000);
+        if(Creature *turret = CAST_CRE(CAST_VEH(me)->GetPassenger(7)))
+            turret->AI()->DoZoneInCombat();
+    }
+
+    // TODO: effect 0 and effect 1 may be on different target
+    void SpellHitTarget(Unit *target, const SpellEntry *spell)
+    {
+        if(spell->Id == SPELL_PURSUED)
+            AttackStart(target);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(!me->isInCombat())
+            return;
+
+        if(me->getThreatManager().isThreatListEmpty())
+        {
+            EnterEvadeMode();
+            return;
+        }
+
+        events.Update(diff);
+
+        if(me->hasUnitState(UNIT_STAT_CASTING))
+            return;
+
+        uint32 eventId = events.GetEvent();
+        if(!me->getVictim())
+            eventId = EVENT_PURSUE;
+
+        switch(eventId)
+        {
+            case 0:
+                return;
+            case EVENT_PURSUE:
+                DoCastAOE(SPELL_PURSUED);
+                events.RepeatEvent(35000);
+                return;
+            case EVENT_MISSILE:
+                //TODO: without unittarget no visual effect
+                //DoCastAOE(SPELL_MISSILE_BARRAGE);
+                DoCast(me->getVictim(), SPELL_MISSILE_BARRAGE);
+                events.RepeatEvent(1500);
+                return;
+            case EVENT_VENT:
+                DoCastAOE(SPELL_FLAME_VENTS);
+                events.RepeatEvent(20000);
+                return;
+            default:
+                events.PopEvent();
+                break;
+        }
+
+        DoSpellAttackIfReady(SPELL_BATTERING_RAM);
+    }
+};
+
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_turretAI : public ScriptedAI
+{
+    boss_flame_leviathan_turretAI(Creature *c) : ScriptedAI(c)
+    {
+        me->SetReactState(REACT_PASSIVE);
+    }
+
+    void Reset()
+    {
+        events.Reset();
+    }
+
+    EventMap events;
+
+    void EnterCombat(Unit *who)
+    {
+        events.ScheduleEvent(1, 5000);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(!UpdateCombatState())
+            return;
+
+        events.Update(diff);
+
+        if(me->hasUnitState(UNIT_STAT_CASTING))
+            return;
+
+        if(uint32 eventId = events.GetEvent())
+        {
+            switch(eventId)
+            {
+                case 1:
+                    DoCast(SelectTarget(SELECT_TARGET_RANDOM), SPELL_CANNON);
+                    events.RepeatEvent(10000);
+                    return;
+                default:
+                    events.PopEvent();
+                    break;
+            }
+        }
+    }
+};
+
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_seatAI : public ScriptedAI
+{
+    boss_flame_leviathan_seatAI(Creature *c) : ScriptedAI(c)
+    {
+        assert(c->isVehicle());
+        if(const CreatureInfo *cInfo = me->GetCreatureInfo())
+            me->SetDisplayId(cInfo->DisplayID_A[0]); // 0 invisible, 1 visible
+    }
+
+    void Reset()
+    {
+        me->SetReactState(REACT_AGGRESSIVE);
+    }
+
+    void MoveInLineOfSight(Unit *who) // for test
+    {
+        if(who->GetTypeId() == TYPEID_PLAYER && !who->m_Vehicle
+            && !CAST_VEH(me)->GetPassenger(0) && CAST_VEH(me)->GetPassenger(2))
+            who->EnterVehicle(CAST_VEH(me), 0);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(!CAST_VEH(me)->GetPassenger(2))
+            if(Unit *who = CAST_VEH(me)->GetPassenger(0))
+                who->ExitVehicle();
+    }
+};
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_defense_turretAI : public ScriptedAI
+{
+    boss_flame_leviathan_defense_turretAI(Creature *c) : ScriptedAI(c)
+    {
+    }
+
+    void Reset()
+    {
+    }
+
+    void DamageTaken(Unit *who, uint32 &damage)
+    {
+        if(!who->m_Vehicle || who->m_Vehicle->GetEntry() != 33114)
+            damage = 0;
+    }
+
+    void MoveInLineOfSight(Unit *who)
+    {
+        if(me->getVictim())
+            return;
+
+        if(who->GetTypeId() != TYPEID_PLAYER || !who->m_Vehicle || who->m_Vehicle->GetEntry() != 33114)
+            return;
+
+        AttackStart(who);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(!UpdateVictim())
+            return;
+
+        if(me->hasUnitState(UNIT_STAT_CASTING))
+            return;
+
+        DoCast(me->getVictim(), SPELL_SEARING_FLAME);
+    }
+};
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_overload_deviceAI : public ScriptedAI
+{
+    boss_flame_leviathan_overload_deviceAI(Creature *c) : ScriptedAI(c)
+    {
+        if(const CreatureInfo *cInfo = me->GetCreatureInfo())
+            me->SetDisplayId(cInfo->DisplayID_H[0]); // A0 gm, H0 device
+    }
+
+    void Reset()
+    {
+    }
+
+    void DamageTaken(Unit *who, uint32 &damage)
+    {
+        if(damage >= me->GetHealth())
+        {
+            damage = 0;
+            if(!me->hasUnitState(UNIT_STAT_CASTING))
+                DoCastAOE(SPELL_OVERLOAD_CIRCUIT);
+        }
+    }
+};
+
+CreatureAI* GetAI_boss_flame_leviathan(Creature *_Creature)
+{
+    return new boss_flame_leviathanAI (_Creature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_turret(Creature *_Creature)
+{
+    return new boss_flame_leviathan_turretAI (_Creature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_seat(Creature *_Creature)
+{
+    return new boss_flame_leviathan_seatAI (_Creature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_defense_turret(Creature *_Creature)
+{
+    return new boss_flame_leviathan_defense_turretAI (_Creature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_overload_device(Creature *_Creature)
+{
+    return new boss_flame_leviathan_overload_deviceAI (_Creature);
+}
+
+void AddSC_boss_flame_leviathan()
+{
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan";
+    newscript->GetAI = &GetAI_boss_flame_leviathan;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_turret";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_turret;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_seat";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_seat;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_defense_turret";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_defense_turret;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_overload_device";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_overload_device;
+    newscript->RegisterSelf();
+}
