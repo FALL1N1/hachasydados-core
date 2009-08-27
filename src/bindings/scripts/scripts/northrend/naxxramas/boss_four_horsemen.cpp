7ddec844490e37e355ac2b55a90878ba225eb21d
megamage <megamage@localhost>
2009-08-27 03:35:53 -0500

    *Fix a crash. Thanks to MaXiMiUS

---
+++ src/bindings/scripts/scripts/northrend/naxxramas/boss_four_horsemen.cpp
@@ -0,0 +1,167 @@
+/* Copyright (C) 2008 - 2009 Trinity <http://www.trinitycore.org/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "precompiled.h"
+#include "def_naxxramas.h"
+
+enum Horsemen
+{
+    HORSEMEN_THANE,
+    HORSEMEN_LADY,
+    HORSEMEN_BARON,
+    HORSEMEN_SIR,
+};
+
+enum Events
+{
+    EVENT_MARK = 1,
+    EVENT_CAST,
+    EVENT_BERSERK,
+};
+
+const uint32 MOB_HORSEMEN[]     =   {16064, 16065, 30549, 16063};
+const uint32 SPELL_MARK[]       =   {28832, 28833, 28834, 28835};
+#define SPELL_PRIMARY(i)            HEROIC(SPELL_PRIMARY_N[i],SPELL_PRIMARY_H[i])
+const uint32 SPELL_PRIMARY_N[]  =   {28884, 28863, 28882, 28883};
+const uint32 SPELL_PRIMARY_H[]  =   {57467, 57463, 57369, 57466};
+#define SPELL_SECONDARY(i)          HEROIC(SPELL_SECONDARY_N[i],SPELL_SECONDARY_H[i])
+const uint32 SPELL_SECONDARY_N[]=   {0, 57374, 0, 57376};
+const uint32 SPELL_SECONDARY_H[]=   {0, 57464, 0, 57465};
+const uint32 SPELL_PUNISH[]     =   {0, 57381, 0, 57377};
+#define SPELL_BERSERK               26662
+
+const int32 SAY_AGGRO[]     =   {-1533051, -1533044, -1533065, -1533058};
+const int32 SAY_TAUNT[3][4] ={  {-1533052, -1533045, -1533071, -1533059},
+                                {-1533053, -1533046, -1533072, -1533060},
+                                {-1533054, -1533047, -1533073, -1533061},};
+const int32 SAY_SPECIAL[]   =   {-1533055, -1533048, -1533070, -1533062};
+const int32 SAY_SLAY[]      =   {-1533056, -1533049, -1533068, -1533063};
+const int32 SAY_DEATH[]     =   {-1533057, -1533050, -1533074, -1533064};
+
+#define SAY_BARON_AGGRO     RAND(-1533065,-1533066,-1533067)
+#define SAY_BARON_SLAY      RAND(-1533068,-1533069)
+
+struct TRINITY_DLL_DECL boss_four_horsemenAI : public BossAI
+{
+    boss_four_horsemenAI(Creature *c) : BossAI(c, BOSS_HORSEMEN)
+    {
+        id = Horsemen(0);
+        for(uint8 i = 1; i < 4; ++i)
+            if (me->GetEntry() == MOB_HORSEMEN[i])
+                id = Horsemen(i);
+        caster = (id == HORSEMEN_LADY || id == HORSEMEN_SIR);
+    }
+
+    Horsemen id;
+    bool caster;
+
+    void MoveInLineOfSight(Unit *who)
+    {
+        BossAI::MoveInLineOfSight(who);
+        if (caster)
+            SelectNearestTarget(who);
+    }
+
+    void AttackStart(Unit *who)
+    {
+        if (caster)
+            AttackStartCaster(who, 20);
+        else
+            BossAI::AttackStart(who);
+    }
+
+    void KilledUnit(Unit* victim)
+    {
+        if (!(rand()%5))
+        {
+            if (id == HORSEMEN_BARON)
+                DoScriptText(SAY_BARON_SLAY, me);
+            else
+                DoScriptText(SAY_SLAY[id], me);
+        }
+    }
+
+    void JustDied(Unit* killer)
+    {
+        _JustDied();
+        DoScriptText(SAY_DEATH[id], me);
+    }
+
+    void EnterCombat(Unit *who)
+    {
+        _EnterCombat();
+        if (id == HORSEMEN_BARON)
+            DoScriptText(SAY_BARON_AGGRO, me);
+        else
+            DoScriptText(SAY_AGGRO[id], me);
+            events.ScheduleEvent(EVENT_MARK, 15000);
+            events.ScheduleEvent(EVENT_CAST, 20000+rand()%5000);
+            events.ScheduleEvent(EVENT_BERSERK, 15*100*1000);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if (!UpdateVictim() || !CheckInRoom())
+            return;
+
+        events.Update(diff);
+
+        if (me->hasUnitState(UNIT_STAT_CASTING))
+            return;
+
+        while(uint32 eventId = events.ExecuteEvent())
+        {
+            switch(eventId)
+            {
+                case EVENT_MARK:
+                    if (!(rand()%5))
+                        DoScriptText(SAY_SPECIAL[id], me);
+                    DoCastAOE(SPELL_MARK[id]);
+                    events.ScheduleEvent(EVENT_MARK, 15000);
+                    return;
+                case EVENT_CAST:
+                    if (!(rand()%5))
+                        DoScriptText(SAY_TAUNT[rand()%3][id], me);
+                    DoCast(SPELL_PRIMARY(id));
+                    events.ScheduleEvent(EVENT_CAST, 15000);
+                    return;
+                case EVENT_BERSERK:
+                    DoScriptText(SAY_SPECIAL[id], me);
+                    DoCast(me, EVENT_BERSERK);
+                    return;
+            }
+        }
+
+        if (!caster)
+            DoMeleeAttackIfReady();
+        else if (!DoSpellAttackIfReady(SPELL_SECONDARY(id)))
+            DoCastAOE(SPELL_PUNISH[id]);
+    }
+};
+
+CreatureAI* GetAI_four_horsemen(Creature* pCreature)
+{
+    return new boss_four_horsemenAI (pCreature);
+}
+
+void AddSC_boss_four_horsemen()
+{
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name = "boss_four_horsemen";
+    newscript->GetAI = &GetAI_four_horsemen;
+    newscript->RegisterSelf();
+}
---
+++ src/bindings/scripts/scripts/northrend/ulduar/ulduar/boss_flame_leviathan.cpp
@@ -0,0 +1,368 @@
+/*
+ * Copyright (C) 2008 - 2009 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "precompiled.h"
+#include "def_ulduar.h"
+#include "Vehicle.h"
+
+
+#define SPELL_PURSUED           62374
+#define SPELL_GATHERING_SPEED   62375
+#define SPELL_BATTERING_RAM     62376
+#define SPELL_FLAME_VENTS       62396
+#define SPELL_MISSILE_BARRAGE   62400
+#define SPELL_SYSTEMS_SHUTDOWN  62475
+
+#define SPELL_FLAME_CANNON      62395
+//#define SPELL_FLAME_CANNON      64692 trigger the same spell
+
+#define SPELL_OVERLOAD_CIRCUIT  62399
+
+#define SPELL_SEARING_FLAME     62402
+
+#define SPELL_BLAZE             62292
+
+#define SPELL_SMOKE_TRAIL       63575
+
+#define SPELL_MIMIRON_INFERNO   62910 // Not Blizzlike
+
+#define SPELL_HODIR_FURY        62297 // Not Blizzlike
+
+enum Mobs
+{
+    MOB_MECHANOLIFT = 33214,
+    MOB_LIQUID      = 33189,
+    MOB_CONTAINER   = 33218,
+};
+
+enum Events
+{
+    EVENT_PURSUE = 1,
+    EVENT_MISSILE,
+    EVENT_VENT,
+    EVENT_SPEED,
+    EVENT_SUMMON,
+    EVENT_MIMIRON_INFERNO, // Not Blizzlike
+    EVENT_HODIR_FURY,      // Not Blizzlike
+};
+
+enum Seats
+{
+    SEAT_PLAYER = 0,
+    SEAT_TURRET = 1,
+    SEAT_DEVICE = 2,
+};
+
+struct TRINITY_DLL_DECL boss_flame_leviathanAI : public BossAI
+{
+    boss_flame_leviathanAI(Creature *c) : BossAI(c, BOSS_LEVIATHAN), vehicle(me->GetVehicleKit())
+    {
+        assert(vehicle);
+    }
+
+    Vehicle *vehicle;
+
+    void Reset()
+    {
+        _Reset();
+        me->SetReactState(REACT_AGGRESSIVE);
+    }
+
+    void EnterCombat(Unit *who)
+    {
+        _EnterCombat();
+        me->SetReactState(REACT_DEFENSIVE);
+        events.ScheduleEvent(EVENT_PURSUE, 0);
+        events.ScheduleEvent(EVENT_MISSILE, 1500);
+        events.ScheduleEvent(EVENT_VENT, 20000);
+        events.ScheduleEvent(EVENT_SPEED, 15000);
+        events.ScheduleEvent(EVENT_SUMMON, 0);
+        //events.ScheduleEvent(EVENT_MIMIRON_INFERNO, 60000 + (rand()%60000)); // Not Blizzlike
+        //events.ScheduleEvent(EVENT_HODIR_FURY, 60000 + (rand()%60000));      // Not Blizzlike
+        if (Creature *turret = CAST_CRE(vehicle->GetPassenger(7)))
+            turret->AI()->DoZoneInCombat();
+    }
+
+    // TODO: effect 0 and effect 1 may be on different target
+    void SpellHitTarget(Unit *target, const SpellEntry *spell)
+    {
+        if (spell->Id == SPELL_PURSUED)
+            AttackStart(target);
+    }
+
+    void SpellHit(Unit *caster, const SpellEntry *spell)
+    {
+        if(spell->Id == 62472)
+            vehicle->InstallAllAccessories();
+    }
+
+    void JustSummoned(Creature *summon)
+    {
+        if(summon->GetEntry() == MOB_MECHANOLIFT)
+        {
+            summons.Summon(summon);
+        }
+    }
+
+    void SummonedCreatureDespawn(Creature *summon)
+    {
+        if(summon->GetEntry() == MOB_MECHANOLIFT)
+        {
+            summons.Despawn(summon);
+            //if(Creature* container = DoSummon(MOB_CONTAINER, summon, 0, 0))
+            //    container->GetMotionMaster()->MovePoint(1, container->GetPositionX(), container->GetPositionY(), me->GetPositionZ());
+        }
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if (!me->isInCombat())
+            return;
+
+        if (me->getThreatManager().isThreatListEmpty())
+        {
+            EnterEvadeMode();
+            return;
+        }
+
+        events.Update(diff);
+
+        if (me->hasUnitState(UNIT_STAT_CASTING))
+            return;
+
+        uint32 eventId = events.GetEvent();
+        if (!me->getVictim())
+            eventId = EVENT_PURSUE;
+
+        switch(eventId)
+        {
+            case 0: break; // this is a must
+            case EVENT_PURSUE:
+                DoCastAOE(SPELL_PURSUED, true);
+                //events.RepeatEvent(35000); // this should not be used because eventId may be overriden
+                events.RescheduleEvent(EVENT_PURSUE, 35000);
+                if(!me->getVictim()) // all siege engines and demolishers are dead
+                    UpdateVictim(); // begin to kill other things
+                return;
+            case EVENT_MISSILE:
+                //TODO: without unittarget no visual effect
+                //DoCastAOE(SPELL_MISSILE_BARRAGE);
+                DoCast(me->getVictim(), SPELL_MISSILE_BARRAGE);
+                events.RepeatEvent(1500);
+                return;
+            case EVENT_VENT:
+                DoCastAOE(SPELL_FLAME_VENTS);
+                events.RepeatEvent(20000);
+                return;
+            case EVENT_SPEED:
+                DoCastAOE(SPELL_GATHERING_SPEED);
+                events.RepeatEvent(15000);
+                return;
+            case EVENT_SUMMON:
+                if(summons.size() < 10)
+                    if(Creature *lift = DoSummonFlyer(MOB_MECHANOLIFT, me, rand()%20 + 20, 50, 0))
+                        lift->GetMotionMaster()->MoveRandom(100);
+                events.RepeatEvent(2000);
+                return;
+            case EVENT_MIMIRON_INFERNO: // Not Blizzlike
+                DoCast(me->getVictim(), SPELL_MIMIRON_INFERNO);
+                events.RepeatEvent(60000 + (rand()%60000));
+                return;
+            case EVENT_HODIR_FURY:      // Not Blizzlike
+                DoCast(me->getVictim(), SPELL_HODIR_FURY);
+                events.RepeatEvent(60000 + (rand()%60000));
+            default:
+                events.PopEvent();
+                break;
+        }
+
+        DoSpellAttackIfReady(SPELL_BATTERING_RAM);
+    }
+};
+
+//#define BOSS_DEBUG
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_seatAI : public PassiveAI
+{
+    boss_flame_leviathan_seatAI(Creature *c) : PassiveAI(c), vehicle(c->GetVehicleKit())
+    {
+        assert(vehicle);
+#ifdef BOSS_DEBUG
+        me->SetReactState(REACT_AGGRESSIVE);
+#endif
+    }
+
+    Vehicle *vehicle;
+
+#ifdef BOSS_DEBUG
+    void MoveInLineOfSight(Unit *who)
+    {
+        if(who->GetTypeId() == TYPEID_PLAYER && CAST_PLR(who)->isGameMaster()
+            && !who->GetVehicle() && vehicle->GetPassenger(SEAT_TURRET))
+            who->EnterVehicle(vehicle, SEAT_PLAYER);
+    }
+#endif
+
+    void PassengerBoarded(Unit *who, int8 seatId)
+    {
+        if(!me->GetVehicle())
+            return;
+
+        if(seatId == SEAT_PLAYER)
+        {
+            if(Creature *turret = CAST_CRE(vehicle->GetPassenger(SEAT_TURRET)))
+            {
+                turret->setFaction(me->GetVehicleBase()->getFaction());
+                turret->SetUInt32Value(UNIT_FIELD_FLAGS, 0); // unselectable
+                turret->AI()->AttackStart(who);
+            }
+            if(Unit *device = vehicle->GetPassenger(SEAT_DEVICE))
+            {
+                device->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_SPELLCLICK);
+                device->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+            }
+        }
+    }
+
+    void PassengerLeft(Unit *who, int8 seatId)
+    {
+        if(seatId == SEAT_TURRET)
+        {
+            if(Unit *device = vehicle->GetPassenger(SEAT_DEVICE))
+            {
+                device->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_SPELLCLICK);
+                device->SetUInt32Value(UNIT_FIELD_FLAGS, 0); // unselectable
+            }
+        }
+    }
+};
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_defense_turretAI : public TurretAI
+{
+    boss_flame_leviathan_defense_turretAI(Creature *c) : TurretAI(c) {}
+
+    void DamageTaken(Unit *who, uint32 &damage)
+    {
+        if(!CanAIAttack(who))
+            damage = 0;
+    }
+
+    bool CanAIAttack(const Unit *who) const
+    {
+        if (who->GetTypeId() != TYPEID_PLAYER || !who->GetVehicle() || who->GetVehicleBase()->GetEntry() != 33114)
+            return false;
+        return true;
+    }
+};
+
+struct TRINITY_DLL_DECL boss_flame_leviathan_overload_deviceAI : public PassiveAI
+{
+    boss_flame_leviathan_overload_deviceAI(Creature *c) : PassiveAI(c) {}
+
+    void DoAction(const int32 param)
+    {
+        if(param == EVENT_SPELLCLICK)
+        {
+            me->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_SPELLCLICK);
+            me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+            if(me->GetVehicle())
+            {
+                if(Unit *player = me->GetVehicle()->GetPassenger(SEAT_PLAYER))
+                {
+                    player->ExitVehicle();
+                    me->GetVehicleBase()->CastSpell(player, SPELL_SMOKE_TRAIL, true);
+                    if(Unit *leviathan = me->GetVehicleBase()->GetVehicleBase())
+                        player->GetMotionMaster()->MoveKnockbackFrom(leviathan->GetPositionX(), leviathan->GetPositionY(), 30, 30);
+                }
+            }                    
+        }
+    }
+};
+
+struct TRINITY_DLL_DECL spell_pool_of_tarAI : public TriggerAI
+{
+    spell_pool_of_tarAI(Creature *c) : TriggerAI(c)
+    {
+        me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+    }
+
+    void DamageTaken(Unit *who, uint32 &damage)
+    {
+        damage = 0;
+    }
+
+    void SpellHit(Unit* caster, const SpellEntry *spell)
+    {
+        if(spell->SchoolMask & SPELL_SCHOOL_MASK_FIRE && !me->HasAura(SPELL_BLAZE))
+            me->CastSpell(me, SPELL_BLAZE, true);
+    }
+};
+
+CreatureAI* GetAI_boss_flame_leviathan(Creature* pCreature)
+{
+    return new boss_flame_leviathanAI (pCreature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_seat(Creature* pCreature)
+{
+    return new boss_flame_leviathan_seatAI (pCreature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_defense_turret(Creature* pCreature)
+{
+    return new boss_flame_leviathan_defense_turretAI (pCreature);
+}
+
+CreatureAI* GetAI_boss_flame_leviathan_overload_device(Creature* pCreature)
+{
+    return new boss_flame_leviathan_overload_deviceAI (pCreature);
+}
+
+CreatureAI* GetAI_spell_pool_of_tar(Creature* pCreature)
+{
+    return new spell_pool_of_tarAI (pCreature);
+}
+
+void AddSC_boss_flame_leviathan()
+{
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan";
+    newscript->GetAI = &GetAI_boss_flame_leviathan;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_seat";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_seat;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_defense_turret";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_defense_turret;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="boss_flame_leviathan_overload_device";
+    newscript->GetAI = &GetAI_boss_flame_leviathan_overload_device;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="spell_pool_of_tar";
+    newscript->GetAI = &GetAI_spell_pool_of_tar;
+    newscript->RegisterSelf();
+}
