70c79c8924a33d60939b2ca60473b74f76ecf120
QAston <QAston@localhost>
2010-02-15 20:59:05 +0100

    *Backout some WTF fixes. Do things correctly, or do not touch them at all... *Really fix windows build.

---
+++ src/scripts/northrend/frozen_halls/pit_of_saron/pit_of_saron.h
@@ -0,0 +1,16 @@
+#ifndef DEF_PIT_OF_SARON_H
+#define DEF_PIT_OF_SARON_H
+enum Data
+{
+    DATA_FORGEMASTER_EVENT,
+    DATA_KRICKANDICK_EVENT,
+    DATA_TYRANNUS_EVENT
+};
+enum Creatures
+{
+    CREATURE_FORGEMASTER                          = 36494,
+    CREATURE_KRICK                                = 36477,
+    CREATURE_ICK                                  = 36476,
+    CREATURE_TYRANNUS                             = 36658
+};
+#endif
