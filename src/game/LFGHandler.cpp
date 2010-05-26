be74e16b75a36e01c866e9c3b3f75fc3cd188fe7
John Holiver <John Holiver@localhost>
2010-05-26 14:54:56 -0300

    Re-re-fix for Life Tap (3.3.3). I hope that this one is the last. Each rank now use diferent spell power gains (0, 0.2, 0.5).

---
+++ src/game/LFGHandler.cpp
@@ -0,0 +1,145 @@
+/*
+ *
+ * Copyright (C) 2008-2010 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "LFGMgr.h"
+#include "WorldSession.h"
+#include "WorldPacket.h"
+#include "Player.h"
+
+void WorldSession::HandleSetLfgCommentOpcode(WorldPacket & recv_data)
+{
+    sLog.outDebug("CMSG_SET_LFG_COMMENT");
+
+    std::string comment;
+    recv_data >> comment;
+
+    GetPlayer()->m_lookingForGroup.comment = comment;
+}
+
+void WorldSession::HandleLfgPlayerLockInfoRequestOpcode(WorldPacket &/*recv_data*/)
+{
+    sLog.outDebug("CMSG_LFD_PLAYER_LOCK_INFO_REQUEST");
+    sLFGMgr.SendLfgPlayerInfo(GetPlayer());
+}
+
+void WorldSession::HandleLfgPartyLockInfoRequestOpcode(WorldPacket &/*recv_data*/)
+{
+    sLog.outDebug("CMSG_LFD_PARTY_LOCK_INFO_REQUEST");
+    sLFGMgr.SendLfgPartyInfo(GetPlayer());
+}
+
+void WorldSession::SendLfgUpdatePlayer(uint8 updateType, uint32 dungeonEntry /* = 0*/)
+{
+    bool queued = false;
+    bool extrainfo = false;
+
+    switch(updateType)
+    {
+    case LFG_UPDATETYPE_JOIN_PROPOSAL:
+    case LFG_UPDATETYPE_ADDED_TO_QUEUE:
+        queued = true;
+        extrainfo = true;
+        break;
+    //case LFG_UPDATETYPE_CLEAR_LOCK_LIST: // TODO: Sometimes has extrainfo - Check ocurrences...
+    case LFG_UPDATETYPE_PROPOSAL_FOUND:
+        extrainfo = true;
+        break;
+    }
+    sLog.outDebug("SMSG_LFG_UPDATE_PLAYER");
+    WorldPacket data(SMSG_LFG_UPDATE_PLAYER, 1 + 1 + (extrainfo ? 1 : 0) * (1 + 1 + 1 + 1 + !dungeonEntry ? 4 : GetPlayer()->m_lookingForGroup.applyDungeons.size() * 4 + GetPlayer()->m_lookingForGroup.comment.length()));
+    data << uint8(updateType);                              // Lfg Update type
+    data << uint8(extrainfo);                               // Extra info
+    if (extrainfo)
+    {
+        data << uint8(queued);                              // Join the queue
+        data << uint8(0);                                   // unk - Always 0
+        data << uint8(0);                                   // unk - Always 0
+
+        if (dungeonEntry)
+        {
+            data << uint8(1);
+            data << uint32(dungeonEntry);
+        }
+        else
+        {
+            uint8 size = GetPlayer()->m_lookingForGroup.applyDungeons.size();
+            data << uint8(size);
+
+            for (LfgDungeonSet::const_iterator it = GetPlayer()->m_lookingForGroup.applyDungeons.begin(); it != GetPlayer()->m_lookingForGroup.applyDungeons.end(); ++it)
+                data << uint32(*it);
+        }
+        data << GetPlayer()->m_lookingForGroup.comment;
+    }
+    SendPacket(&data);
+}
+
+void WorldSession::SendLfgUpdateParty(uint8 updateType, uint32 dungeonEntry /* = 0*/)
+{
+    bool join = false;
+    bool extrainfo = false;
+    bool queued = false;
+
+    switch(updateType)
+    {
+    case LFG_UPDATETYPE_JOIN_PROPOSAL:
+        extrainfo = true;
+        break;
+    case LFG_UPDATETYPE_ADDED_TO_QUEUE:
+        extrainfo = true;
+        join = true;
+        queued = true;
+        break;
+    case LFG_UPDATETYPE_CLEAR_LOCK_LIST:
+        // join = true;  // TODO: Sometimes queued and extrainfo - Check ocurrences...
+        queued = true;
+        break;
+    case LFG_UPDATETYPE_PROPOSAL_FOUND:
+        extrainfo = true;
+        join = true;
+        break;
+    }
+
+    sLog.outDebug("SMSG_LFG_UPDATE_PARTY");
+    WorldPacket data(SMSG_LFG_UPDATE_PARTY, 1 + 1 + (extrainfo ? 1 : 0) * (1 + 1 + 1 + 1 + 1 + !dungeonEntry ? 4 : GetPlayer()->m_lookingForGroup.applyDungeons.size() * 4 + GetPlayer()->m_lookingForGroup.comment.length()));
+    data << uint8(updateType);                              // Lfg Update type
+    data << uint8(extrainfo);                               // Extra info
+    if (extrainfo)
+    {
+        data << uint8(join);                                // LFG Join
+        data << uint8(queued);                              // Join the queue
+        data << uint8(0);                                   // unk - Always 0
+        data << uint8(0);                                   // unk - Always 0
+
+        if (dungeonEntry)
+        {
+            data << uint8(1);
+            data << uint32(dungeonEntry);
+        }
+        else
+        {
+            uint8 size = GetPlayer()->m_lookingForGroup.applyDungeons.size();
+            data << uint8(size);
+
+            for (LfgDungeonSet::const_iterator it = GetPlayer()->m_lookingForGroup.applyDungeons.begin(); it != GetPlayer()->m_lookingForGroup.applyDungeons.end(); ++it)
+                data << uint32(*it);
+        }
+        data << GetPlayer()->m_lookingForGroup.comment;
+    }
+    SendPacket(&data);
+}
