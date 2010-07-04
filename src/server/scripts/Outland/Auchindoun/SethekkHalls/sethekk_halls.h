41d80dd6d2022503a75e42a73687ccd4a78ff636
teacher <teacher@localhost>
2010-07-04 13:59:57 +0200

    Added serverside spell place holder for future development. Added missing data to world_database.sql. Corrected a couple of typos in world_script_texts.sql.

---
+++ src/server/scripts/Outland/Auchindoun/SethekkHalls/sethekk_halls.h
@@ -0,0 +1,15 @@
+/* Copyright (C) 2008 - 2010 Trinity <http://www.trinitycore.org/>
+ * Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_SETHEKK_HALLS_H
+#define DEF_SETHEKK_HALLS_H
+
+enum eTypes
+{
+    DATA_IKISSDOOREVENT = 1,
+    TYPE_ANZU_ENCOUNTER = 2,
+};
+#endif
+
